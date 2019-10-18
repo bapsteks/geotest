a4b0f22208ec043deafe956f1fb54f41d891ac6f
Chris Wright <bapsteks@gmail.com>
2019-10-18 14:38:36 +0100

    Geo test copy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {
+  Component
+} from '@angular/core';
+import BackgroundGeolocation from 'cordova-background-geolocation-lt';
+import {
+  Platform
+} from '@ionic/angular';
+import {
+  SplashScreen
+} from '@ionic-native/splash-screen/ngx';
+import {
+  StatusBar
+} from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
@@ -19,9 +27,29 @@ export class AppComponent {
   }
 
   initializeApp() {
+    const bgGeo = BackgroundGeolocation;
+
+    this.platform.resume.subscribe(() => {
+      console.log('Resumed app');
+    });
+
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      bgGeo.ready({
+        reset: true,
+        debug: true,
+        logLevel: bgGeo.LOG_LEVEL_VERBOSE,
+        desiredAccuracy: bgGeo.DESIRED_ACCURACY_HIGH,
+        distanceFilter: 10,
+        url: 'http://my.server.com/locations',
+        autoSync: true,
+        stopOnTerminate: false,
+        startOnBoot: true
+      }, function () {
+        console.log('Plugin configured.. running start function now.');
+        bgGeo.start();
+      });
     });
   }
 }
